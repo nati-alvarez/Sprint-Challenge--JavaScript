9ae36302f4a20a6d01971dfc326cd93354e0c0a8
Natividad Alvarez <tivialvarez@gmail.com>
2020-05-15 19:47:15 -0400

    Completed prototypes.js

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,25 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+/*
+nestedFunction has access to the variable internal, because the function declaration lies in the same lexical environment as the variable declaration
+of internal. First nestedFunction would look in its own scope to find a variable named internal. However there isn't one.
+so therefore, nestedFunction will look up it's scope chain to find internal, much like myFunction has access to the global variable external.
+*/
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function sumation(param){
+  let sum = 0;
+  function counter(){
+    for(let i = param; i > 0; i--){
+      sum += i;
+    }
+  }
+  counter();
+  return sum;
+}
+
+console.log(sumation(4));
